840d38c1467c255cd278ae65e25554f24e47084d
Satyam Lal <satyamlal@users.noreply.github.com>
2025-06-28 12:55:15 +0530

    Locomotive scrollTrigger added with GSAP

--- P6 - Locomotive+ScrollTrigger using GSAP/script.js
+++ P6 - Locomotive+ScrollTrigger using GSAP/script.js
@@ -1,27 +1,79 @@
-var allHeadings = document.querySelectorAll("#page2 h1");
+function locomotiveScroller() {
+  gsap.registerPlugin(ScrollTrigger);
 
-allHeadings.forEach(function (elem) {
-  var h1Text = elem.textContent;
-  var splittedText = h1Text.split("");
+  // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
 
-  var clutter = "";
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true,
+  });
+  // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+  locoScroll.on("scroll", ScrollTrigger.update);
 
-  splittedText.forEach(function (e) {
-    clutter += `<span>${e}</span>`;
+  // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+  ScrollTrigger.scrollerProxy("#main", {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+    },
+    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+    pinType: document.querySelector("#main").style.transform
+      ? "transform"
+      : "fixed",
   });
 
-  elem.innerHTML = clutter;
-});
+  // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 
-gsap.to("#page2 h1 span", {
-  color: "#e3e3c4",
-  stagger: 0.1,
-  scrollTrigger: {
-    trigger: "#page2",
-    scroller: "body",
-    markers: true,
-    start: "top 50%",
-    end: "top -5%",
-    scrub: 1,
-  },
-});
+  // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+  ScrollTrigger.refresh();
+}
+
+function textSplitting() {
+  var allHeadings = document.querySelectorAll("#page2 h1");
+
+  allHeadings.forEach(function (elem) {
+    var h1Text = elem.textContent;
+    var splittedText = h1Text.split("");
+
+    var clutter = "";
+
+    splittedText.forEach(function (e) {
+      clutter += `<span>${e}</span>`;
+    });
+
+    elem.innerHTML = clutter;
+  });
+}
+
+function gsapAnimation() {
+  gsap.to("#page2 h1 span", {
+    color: "#e3e3c4",
+    stagger: 0.1,
+    scrollTrigger: {
+      trigger: "#page2 h1",
+      scroller: "#main",
+      markers: true,
+      start: "top 50%",
+      end: "top -5%",
+      scrub: 1,
+    },
+  });
+}
+
+function trigger() {
+  locomotiveScroller();
+  textSplitting();
+  gsapAnimation();
+}
+
+trigger();
